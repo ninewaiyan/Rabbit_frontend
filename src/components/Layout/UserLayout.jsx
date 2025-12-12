a51cf8ed0203978cf49d97ec5777d48d918455e5
ninenine <ninewaiyan@evercomm.services>
2025-12-12 15:42:17 +0630

    footer

--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Header from '../Common/Header'
+import React from "react";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 const UserLayout = () => {
   return (
     <>
-    {/** Header */}
-    <Header/>
-    {/** Main Content*/}
-    {/** Footer*/}
+      {/** Header */}
+      <Header />
 
+      {/** Main Content*/}
+      <main>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo soluta
+        labore, nihil temporibus, explicabo laboriosam tempore aperiam non,
+        consectetur hic sit odio exercitationem facilis quidem! Velit inventore
+        tenetur qui fuga earum aliquam, in facere unde nemo nisi, quam natus
+        deserunt harum iure eius itaque nam perferendis quo, officiis tempora
+        sed reiciendis nulla. Odit laudantium autem illo itaque ex incidunt et
+        sit quod rerum iure aspernatur dolorem, non fuga delectus cupiditate est
+        consequuntur magnam tempora repellendus alias sequi doloribus?
+        Aspernatur expedita iure, illum inventore exercitationem pariatur nam
+        minima illo corrupti eos maxime quisquam maiores excepturi aliquid
+        reiciendis magni eligendi similique aliquam.
+      </main>
+      {/** Footer*/}
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
