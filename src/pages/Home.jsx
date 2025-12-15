009cdaa8598a5e4cb8a113ef11b4bedb8d409c11
ninenine <ninewaiyan@evercomm.services>
2025-12-16 01:01:55 +0630

    best seller

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivals from '../components/Products/NewArrivals'
+import ProductDetails from '../components/Products/ProductDetails'
 
 const Home = () => {
   return (
@@ -12,6 +13,10 @@ const Home = () => {
             <GenderCollectionSection/>
 
             <NewArrivals/>
+
+            {/* Best Seller */}
+            <h2 className='text-3xl text-center font-bold mb-4 '>Best Seller</h2>
+            <ProductDetails/>
 
 
     </div>
